84f76bc61a46fc1bd92179ccd5be3483ca4d22fa
SongSang <3286271980@qq.com>
2026-09-03 20:52:39 +0800

    initial hello demo

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(b"hello jack")
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+print("Server started on port 8080")
+server.serve_forever()
